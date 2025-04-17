ed2dcc3902049d1b6d96f96ad062f7d1cc354a84
Anas Hasan <anas.hassan9417@gmail.com>
2025-04-17 17:46:15 +0530

    Added logic for axios instance handling

---
+++ frontend/src/api/axiosInstance.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+  
+  // Create an axios instance with baseURL
+  const axiosInstance = axios.create({
+      baseURL: "http://127.0.0.1:8000/api",
+  });
+  
+  // The token will be set dynamically in the component instead of the axiosInstance
+  const setAuthorizationToken = (token) => {
+      if (token) {
+          axiosInstance.defaults.headers.Authorization = `Bearer ${token}`;
+      } else {
+          console.warn("No auth token found! Requests may be unauthorized.");
+      }
+  };
+  
+  export { axiosInstance, setAuthorizationToken };
